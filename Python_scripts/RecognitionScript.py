a6030ff981574ffda60d0a40596c1f5c35989621
root <root@nu.takoe>
2018-12-08 20:05:11 +0300

    Last

--- Python_scripts/RecognitionScript.py
+++ Python_scripts/RecognitionScript.py
@@ -13,7 +13,7 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 id = 0
 
 # names related to ids: example ==> Marcelo: id=1,  etc
-names = ['None', 'Oh well i know you', '1', '2', '3', '4'] 
+names = ['None', 'Peter', 'Kirill', 'Ivan', 'Dmitriy'] 
 
 # Initialize and start realtime video capture
 cam = cv2.VideoCapture(0)
@@ -49,9 +49,9 @@ while True:
             id = names[id]
             confidence = "  {0}%".format(round(100 - confidence))
         else:
-            id = "unknown"
+            id = "unknown person"
             confidence = "  {0}%".format(round(100 - confidence))
-        
+
         cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
         cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
     
